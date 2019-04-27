043660df37ae03fc0b0d279c43e0ae2543b22281
Jasha Joachimsthal <jashaj@gmail.com>
2019-04-27 12:00:20 +0200

    img/@alt is required but must be empty when decorative

--- src/components/restaurant/RestaurantItem.js
+++ src/components/restaurant/RestaurantItem.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { List, Image, Rating } from 'semantic-ui-react';
+import {Image, List, Rating} from 'semantic-ui-react';
 
 export default class RestaurantItem extends React.Component {
 
-  render () {
+  render() {
     return (
         <List.Item>
-          <Image avatar src={this.props.restaurant.image} />
-              <List.Content>
-              <List.Header>{this.props.restaurant.name}</List.Header>
-          Rating: <Rating icon='heart' defaultRating={this.props.restaurant.rating} maxRating={5} />
+          <Image avatar src={this.props.restaurant.image} alt=''/>
+          <List.Content>
+            <List.Header>{this.props.restaurant.name}</List.Header>
+            Rating: <Rating icon='heart' defaultRating={this.props.restaurant.rating} maxRating={5}/>
           </List.Content>
         </List.Item>
     );
